3560365a05f2a731d01d83b4a3362c46145c4f71
Phillyp Henning <phil8t8@gmail.com>
2019-01-08 17:17:05 -0500

    Up to 3 for me

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
+// =+Modules+=
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
-const app = express();
 
+// =+Configuration+=
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public'))); // used to set css/js scripts directory
 
-// used to set css/js scripts directory
-app.use(express.static(path.join(__dirname, 'public')));
+const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
 
+const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), 'utf8');
+const users = JSON.parse(userData);
+
+// =+Routes+=
 app.get('/', (req, res) =>{
-    res.render('index', {title: 'Index'});
+    res.render('index', {title: 'Account Summary', accounts: accounts});
 });
 
 app.listen(3000, () => console.log('Project running on port 3000...'));
