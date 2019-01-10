fa746438325db77cb31618567fcfb34b1ce48557
Phillyp Henning <phil8t8@gmail.com>
2019-01-10 11:37:48 -0500

    Cleanup and adding transfer route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // =+Modules+=
 const fs = require('fs');
 const path = require('path');
+const util = require('util'); // Provides timestamp
 const express = require('express');
 
 
@@ -10,32 +11,25 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public'))); // used to set css/js scripts directory
+app.use(express.urlencoded({ extended: true })); // https://expressjs.com/en/api.html#express.urlencoded
 
-const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), 'utf8');
+const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), 'utf8'); 
 const accounts = JSON.parse(accountData);
 
 const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), 'utf8');
 const users = JSON.parse(userData);
 
 // =+Routes+=
-app.get('/', (req, res) =>{
-    res.render('index', {title: 'Account Summary', accounts});
-});
+app.get('/', (req, res) =>{ res.render('index', {title: 'Account Summary', accounts}); });
 
-app.get('/savings', (req, res) => {
-    res.render('account', {account: accounts.savings});
-});
+app.get('/savings', (req, res) => { res.render('account', { account: accounts.savings }); });
 
-app.get('/checking', (req, res) => {
-    res.render('account', {account: accounts.checking});
-});
+app.get('/checking', (req, res) => { res.render('account', { account: accounts.checking }); });
 
-app.get('/credit', (req, res) => {
-    res.render('account', {account: accounts.credit});
-});
+app.get('/credit', (req, res) => { res.render('account', { account: accounts.credit }); });
 
-app.get('/profile', (req, res) =>{
-    res.render('profile', {user: users[0]});
-});
+app.get('/profile', (req, res) =>{ res.render('profile', { user: users[0] }); });
 
-app.listen(3000, () => console.log('Project running on port 3000...'));
+app.get('/transfer', (req, res) => { res.render('transfer'); });
+
+app.listen(3000, () => util.log('Project running on port 3000...'));
