2ff8cfa3534f1e1e2e3ff06dc8bfda622f36c6ba
Phillyp Henning <phil8t8@gmail.com>
2019-01-11 12:13:52 -0500

    Created data library

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const util = require('util'); // Provides timestamp
 const express = require('express');
-
+const { accounts, users, writeJSON } = require('./data');
 
 // =+Configuration+=
 const app = express();
@@ -13,11 +13,6 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public'))); // used to set css/js scripts directory
 app.use(express.urlencoded({ extended: true })); // https://expressjs.com/en/api.html#express.urlencoded
 
-const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), 'utf8'); 
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), 'utf8');
-const users = JSON.parse(userData);
 
 // =+Routes+=
 app.get('/', (req, res) =>{ res.render('index', {title: 'Account Summary', accounts}); });
@@ -43,8 +38,7 @@ app.post('/transfer', (req, res) => {
     accounts[ta].balance = parseInt(accounts[ta].balance + req.body.amount, 10);
 
     var accountsJSON = JSON.stringify(accounts, null, 4);
-
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('transfer', { message: 'Transfer Completed' });
 });
 
@@ -52,10 +46,9 @@ app.post('/payment', (req, res) => {
     accounts.credit.balance = parseInt(accounts.credit.balance - req.body.amount, 10);
     accounts.credit.available = parseInt(accounts.credit.available + req.body.amount, 10);
 
-    var accountsJSON = JSON.stringify(accounts, null, 4);
-
-    fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('payment', { message: 'Payment Successful!', account: accounts.credit });
 });
+
 
 app.listen(3000, () => util.log('Project running on port 3000...'));
